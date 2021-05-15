ff86e7db9b4682326d157cc505a1104c6722abd3
Joe Sarsfield <joe.sarsfield@gmail.com>
2021-05-15 11:35:27 +0100

    Added add todolist button.

--- todolistapp/views_index.py
+++ todolistapp/views_index.py
@@ -6,7 +6,15 @@ __email__ = "joe.sarsfield@gmail.com"
 """
 
 from django.shortcuts import render, HttpResponseRedirect
+from .models import TodoList, Task
 
 
 def index(request):
+    if request.method == "POST":
+        action = request.POST.get("action", "None")
+        if action == "AddTodo":
+            title = request.POST.get("title", None)
+            if title:
+                TodoList.objects.create(title=title)
+            return HttpResponseRedirect("/")
     return render(request, "index.html", {"todolist": None, "tasks": None})
